09d6edf27d333c172ac9b3762475f3c0a7e077ec
Roman <s3698599@gmail.com>
2025-10-10 23:20:18 +0300

    add script.js and folder static. edit forms.py

--- cart/forms.py
+++ cart/forms.py
@@ -3,21 +3,12 @@ from .models import CartItem
 
 
 class AddToCartForm(forms.Form):
-    size_id = forms.IntegerField(required=False)
+    size_id = forms.IntegerField(required=True)
     quantity = forms.IntegerField(min_value=1, initial=1)
 
     def __init__(self, *args, product=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.product = product
-
-        if product:
-            sizes = product.product_sizes.size(stock__gt=0)
-            if sizes.exists():
-                self.fields['size_id'] = forms.ChoiceField(
-                    choices=[(ps.id, ps.name) for ps in sizes],
-                    required=True,
-                    initial=sizes.first().id
-                )
 
 
 
